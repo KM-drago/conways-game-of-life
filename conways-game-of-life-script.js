0d17405590972491411deaba2892d1a24428eb65
KM-drago <62215420+KM-drago@users.noreply.github.com>
2024-06-28 14:22:48 +0530

    feat(cell coloring)

--- conways-game-of-life-script.js
+++ conways-game-of-life-script.js
@@ -1,5 +1,19 @@
 document.addEventListener("load", gridDraw());
 
+let leftMouseDown = false;
+
+document.addEventListener("mousedown", function (event) {
+  if (event.button === 0) {
+    leftMouseDown = true;
+  }
+});
+
+document.addEventListener("mouseup", function (event) {
+  if (event.button === 0) {
+    leftMouseDown = false;
+  }
+});
+
 function gridDraw() {
   let gSizex = document.getElementById("gSizex").value;
 
@@ -8,7 +22,6 @@ function gridDraw() {
   }
   document.getElementById("gSizex").value = gSizex;
   document.getElementById("gSizey").value = gSizex;
-  console.log("GG");
 
   let gridCanvas = document.getElementById("gridCanvas");
 
@@ -16,7 +29,7 @@ function gridDraw() {
   for (let i = 0; i < gSizex; i++) {
     let tableRow = "<tr>";
     for (let j = 0; j < gSizex; j++) {
-      tableRow += `<td id="cell-${i}-${j}" class="border-solid border border-zinc-950 bg-teal-300"></td>`;
+      tableRow += `<td id="${i}-${j}" class="border-solid border border-zinc-950 bg-teal-300" onclick="clickCellColor(this.id)" onmouseover="dragCellColor(this.id)"></td>`;
     }
     tableRow += "<tr>";
     table += tableRow;
@@ -25,6 +38,24 @@ function gridDraw() {
   gridCanvas.innerHTML = table;
 }
 
-//  let rc = document.getElementById("cell-50-50");
-//  rc.classList.remove("bg-teal-300");
-//  rc.classList.add("bg-teal-950");
+function dragCellColor(id) {
+  if (leftMouseDown) {
+    let rc = document.getElementById(id);
+    rc.classList.remove("bg-teal-300");
+    rc.classList.add("bg-teal-950");
+  }
+}
+
+function clickCellColor(id) {
+  let rc = document.getElementById(id);
+  rc.classList.remove("bg-teal-300");
+  rc.classList.add("bg-teal-950");
+}
+
+function start() {
+  console.log("RUN");
+}
+
+function stop() {
+  console.log("STOP");
+}
